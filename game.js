e442b511fa85d776344c32bcc2cfe0b47358f571
heayr <79095992+heayr@users.noreply.github.com>
2022-03-29 01:49:22 +0300

    mark_3

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 // console.log(choices);
 
@@ -16,14 +18,34 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json')
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+)
     .then((res) => {
         return res.json();
     })
     .then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            })
+
+            return formattedQuestion;
+
+        });
+        // questions = loadedQuestions;
+
         startGame();
+
 
     })
     .catch(err => {
@@ -35,14 +57,15 @@ fetch('questions.json')
 // constants 
 
 const correctBonus = 10;
-const maxQuestions = 3;
+const maxQuestions = 10;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log();
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 getNewQuestion = () => {
 
